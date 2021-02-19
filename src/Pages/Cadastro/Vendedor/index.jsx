a4c93ac84b1e1d742aa46ac7c6d2b32e44cbc339
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-18 21:13:38 -0300

    up

--- src/Pages/Cadastro/Vendedor/index.jsx
+++ src/Pages/Cadastro/Vendedor/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { enviarDados } from "../../../functions/database";
+import { enviarDados } from "../../../functions/bancoDeDados";
 
 const CadastroVendedor = () => {
     const [dados,setDados] = React.useState('')
