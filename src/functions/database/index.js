c2467678b19edebd9e8085edceb7d09118db4eb5
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-19 02:34:26 -0300

    up

--- src/functions/database/index.js
+++ src/functions/database/index.js
@@ -3,7 +3,7 @@ export const enviarDados = (url,form,setDados) => event => {
     
     fetch(url,{
         method: "POST",
-        headers: { "Content-Type": "application/json", "Accept": "application/json","Access-Control-Allow-Origin": "*"},
+        headers: { "Content-Type": "application/json", "Accept": "application/json","Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true, "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS", "Access-Control-Allow-Headers": "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json" },
         body: JSON.stringify(form)
     }).then(response => response.json()).then(dados => {
         setDados(dados);
