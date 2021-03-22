d73b7400861e5a75451d462e983a448903dcbafe
Gabriela Vespoli <gabiat3@hotmail.com>
2021-03-21 21:53:20 -0300

    up content-type

--- src/functions/database/index.js
+++ src/functions/database/index.js
@@ -1,16 +1,21 @@
-export const enviarDados = (url,form,setDados,method = 'POST') => event => {    
-    event.preventDefault();
-    
-    fetch(url,{
-        method: method,
-        headers: { "Content-Type": "application/json", "Accept": "application/json" },
-        body: JSON.stringify(form)
-    }).then(response => response.json()).then(dados =>setDados(dados))
-}
+export const enviarDados = (url, form, setDados, method = "POST") => (
+  event
+) => {
+  event.preventDefault();
 
-export const pegarDados = (url,setData) => {
-    async function fetchData() {
-        const response = await fetch(url);
-        setData(await response.json());
-    } fetchData();    
-}
+  fetch(url, {
+    method: method,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(form),
+  })
+    .then((response) => response.json())
+    .then((dados) => setDados(dados));
+};
+
+export const pegarDados = (url, setData) => {
+  async function fetchData() {
+    const response = await fetch(url);
+    setData(await response.json());
+  }
+  fetchData();
+};
